73575f4ff80d2abda682b9b7e80d5cec369211ff
Aditya kumar <ak20201605@gmail.com>
2022-09-12 14:38:58 +0530

    12-9-2002

---
+++ DAY-2/array.js
@@ -0,0 +1,27 @@
+/*
+let arr = ["Mango", {
+    name: "Aditya Kumar"
+}, 76, true, function () {
+    console.log("hello");
+}];
+console.log(`Element at index 0 :- ${arr[0]} \nElement at index 1 :- ${arr[1].name} \nElement at index 2 :- ${arr[2]} \nElement at indeex 3 :- ${arr[3]}`);
+
+arr[4]();
+
+console.log(arr.at(-4).name);
+
+
+
+//PUSH and POP
+let arr = ['Mango', 76, 'Aditya Kumar'];
+arr[1] = 50;
+let remove = arr.shift();
+console.log(arr.at(-1));
+
+*/
+
+//searching
+
+let arr = [10, 20, 40, 50, 'Aditya', true];
+
+console.log(arr.indexOf(50));
